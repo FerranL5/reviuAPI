9689ae8c590e7b4f1724211fc00641a7f4ba76ad
FerranL5 <ferran_lorcagallardo@iescarlesvallbona.cat>
2025-04-02 19:10:47 +0200

    Update resultatBusquedaNom.cs

--- reviuAPI/Models/resultatBusquedaNom.cs
+++ reviuAPI/Models/resultatBusquedaNom.cs
@@ -3,20 +3,22 @@
     public class resultatBusquedaNom
     {
 
-      public bool? adult { get; set; }
-      public string? backdrop_path { get; set; }
-      public int id { get; set; }
-      public string title { get; set; }
-      public string original_title {  get; set; }
-      public string overview {  get; set; }
-      public string poster_path { get; set; }
-      public string media_type { get; set; } // movie o tv
-      public string original_language { get; set; }
-      public List<int> genre_ids { get; set; }
-      public float popularity { get; set; }
-      public string release_date { get; set; }
-      public float vote_average { get; set; }
-      public int vote_count { get; set; }
+        public bool? adult { get; set; }
+        public string? backdrop_path { get; set; }
+        public int id { get; set; }
+        public string? title { get; set; }
+        public string? original_title {  get; set; }
+        public string? name { get; set; }
+        public string? original_name { get; set; }
+        public string overview {  get; set; }
+        public string poster_path { get; set; }
+        public string media_type { get; set; } // movie o tv
+        public string original_language { get; set; }
+        public List<int> genre_ids { get; set; }
+        public float popularity { get; set; }
+        public string release_date { get; set; }
+        public float vote_average { get; set; }
+        public int vote_count { get; set; }
     
 
 
